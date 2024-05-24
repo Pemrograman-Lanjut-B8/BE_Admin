59dad798ef68f7fd55db73afac63ecb6b7264259
DimasHN_04 <dimashn04@gmail.com>
2024-05-24 20:10:15 +0700

    [REFACTOR] Fix test

--- src/main/java/id/ac/ui/cs/advprog/admin/AdminApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/AdminApplication.java
@@ -2,9 +2,8 @@ package id.ac.ui.cs.advprog.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class AdminApplication {
 
     public static void main(String[] args) {
