640de9b3e124f64facdb7f49383ee2328f5a9090
DimasHN_04 <dimashn04@gmail.com>
2024-05-24 17:29:20 +0700

    [GREEN] Implemented book service

--- src/main/java/id/ac/ui/cs/advprog/admin/service/BookServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/admin/service/BookServiceImpl.java
@@ -1,11 +1,11 @@
 package id.ac.ui.cs.advprog.admin.service;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import id.ac.ui.cs.advprog.admin.model.Book;
 import id.ac.ui.cs.advprog.admin.repository.BookRepository;
@@ -16,51 +16,54 @@ public class BookServiceImpl implements BookService {
     private BookRepository bookRepository;
 
     @Override
-    @Async
-    public CompletableFuture<Book> createBook(Book book) {
-        return CompletableFuture.completedFuture(bookRepository.save(book));
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
     }
 
     @Override
-    @Async
-    public CompletableFuture<List<Book>> findAll() {
-        return CompletableFuture.completedFuture(bookRepository.findAll());
+    public List<Book> findAll() {
+        return bookRepository.findAll();
     }
 
     @Override
-    @Async
-    public CompletableFuture<Book> findByIsbn(String isbn) {
-        return CompletableFuture.completedFuture(bookRepository.findByIsbn(isbn));
+    public Book findByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn);
     }
 
     @Override
-    @Async
-    public CompletableFuture<Book> update(String isbn, Book newBook) {
-        Book book = bookRepository.findByIsbn(isbn);
-        if (book != null) {
-            book.setJudulBuku(newBook.getJudulBuku());
-            book.setPenulis(newBook.getPenulis());
-            book.setPenerbit(newBook.getPenerbit());
-            book.setDeskripsi(newBook.getDeskripsi());
-            book.setHarga(newBook.getHarga());
-            book.setStok(newBook.getStok());
-            book.setTanggalTerbit(newBook.getTanggalTerbit());
-            book.setJumlahHalaman(newBook.getJumlahHalaman());
-            book.setFotoCover(newBook.getFotoCover());
-            book.setKategori(newBook.getKategori());
-            book.setRating(newBook.getRating());
-            return CompletableFuture.completedFuture(bookRepository.save(book));
-        }
-        return CompletableFuture.completedFuture(null);
+    @Transactional
+    public void update(String isbn, 
+        String judulBuku, 
+        String penulis, 
+        String penerbit, 
+        String deskripsi, 
+        double harga, 
+        int stok, 
+        LocalDate tanggalTerbit, 
+        int jumlahHalaman, 
+        String fotoCover, 
+        String kategori, 
+        double rating
+    ) {
+        bookRepository.update(
+            judulBuku, 
+            penulis, 
+            penerbit, 
+            deskripsi, 
+            harga, 
+            stok, 
+            tanggalTerbit, 
+            jumlahHalaman, 
+            fotoCover, 
+            kategori, 
+            rating, 
+            isbn);
+
+        bookRepository.flush();
     }
 
     @Override
-    @Async
-    public CompletableFuture<Void> deleteByIsbn(String isbn) {
-        Book book = bookRepository.findByIsbn(isbn);
-        if (book != null) {
-            bookRepository.delete(book);
-        }
-        return CompletableFuture.completedFuture(null);
+    public void deleteByIsbn(String isbn) {
+        bookRepository.delete(bookRepository.findByIsbn(isbn));
     }
 }
